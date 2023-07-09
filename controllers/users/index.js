179c27bd376b3af637c911bd447123fec8daef09
IvanTolmachev <39563900+IvanTolmachev@users.noreply.github.com>
2023-07-09 04:36:09 +0300

    hw6

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -4,12 +4,17 @@ const logout = require("./logout");
 const getUsersData = require("./getUserDate");
 const updateSubscription = require("./updateSubscription");
 const updateAvatar = require("./updateAvatar");
+const resendVerificationEmail = require("./resendVerificationEmail");
+const verificationEmail = require("./verificationEmail");
+const { controllerWrapper } = require("../../utils/index");
 
 module.exports = {
-  register,
+  register: controllerWrapper(register),
   login,
   logout,
   getUsersData,
   updateSubscription,
   updateAvatar,
+  resendVerificationEmail: controllerWrapper(resendVerificationEmail),
+  verificationEmail: controllerWrapper(verificationEmail),
 };
